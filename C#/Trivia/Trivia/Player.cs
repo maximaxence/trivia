313419c3bca4590cb3a2e169f347aaa40c1c2558
Villand <p1303207@IUTDOUAB2PC4.univ-lyon1.fr>
2016-03-31 14:46:34 +0200

    Player

--- C#/Trivia/Trivia/Player.cs
+++ C#/Trivia/Trivia/Player.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace UglyTrivia
 {
     internal class Player
@@ -5,8 +8,28 @@ namespace UglyTrivia
         public Player(string playerName)
         {
             Name = playerName;
+            Purse = 0;
+            Place = 0;
         }
 
         public string Name { get; private set; }
+        public int Purse { get; private set; }
+        public int Place { get; private set; }
+
+        public class Players
+        {
+            List<Player> players = new List<Player>();
+
+            public List<Player> add(Player player)
+            {
+                players.Add(player);
+                return players;
+            }
+
+            public Player Player(int numPlayer)
+            {
+                return players[numPlayer];
+            }
+        }
     }
 }
